cca5e8a07038b5d3ff84da666cf34d96508304ce
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2024-03-05 22:04:48 +0100

    added _setup.sh

---
+++ _setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pyenv local 3.11
+module load cuda12.3/toolkit/12.3
--- retrieval.sh
+++ retrieval.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source _setup.sh
-nohup python -u exp/retrieval.py $1 $2 &> "logs/retrieval_$1_$2.out" &
+nohup python -u exp/retrieval.py "$1" "$2" &> "logs/retrieval_$1_$2.out" &
